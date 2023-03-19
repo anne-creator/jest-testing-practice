0eec314be5d2ab481b9278b2651434a546b1ab39
Anne <anneliu49@gmail.com>
2023-03-19 04:08:37 -0400

    did checkbox and submit button in  SummaryForm

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
 
-  const checkboxLabel = (
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
+  const checkBoxLabel = (
     <span>
       I agree to
-      <span style={{ color: "blue" }}> Terms and Conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
@@ -18,8 +28,8 @@ export default function SummaryForm() {
         <Form.Check
           type="checkbox"
           checked={tcChecked}
-          onChange={(e) => setTcChecked(e.target.checked)}
-          label={checkboxLabel}
+          onChange = {(e) -> setTcCheckt(e.target.checked)}
+          label={checkedBoxLabel} //displayed next to the checkbox
         />
       </Form.Group>
       <Button variant="primary" type="submit" disabled={!tcChecked}>
